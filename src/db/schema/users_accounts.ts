9c28f8d012aea6c24e1d960a17755296b41e6f1e
Jake Holman <me@jakeisonline.com>
2024-12-18 21:30:07 +0000

    only one owner per account

--- src/db/schema/users_accounts.ts
+++ src/db/schema/users_accounts.ts
@@ -7,11 +7,18 @@ import {
   uniqueIndex,
   pgEnum,
 } from "drizzle-orm/pg-core"
+import { eq, sql } from "drizzle-orm"
 
 export const usersAccountStatuses = pgEnum("user_account_statuses", [
   "pending",
   "active",
   "inactive",
+])
+
+export const userAccountsRoles = pgEnum("user_account_roles", [
+  "owner",
+  "admin",
+  "user",
 ])
 
 export const usersAccountsTable = table(
@@ -23,6 +30,7 @@ export const usersAccountsTable = table(
     userId: text("user_id").references(() => usersTable.id),
     accountId: text("account_id").references(() => accountsTable.id),
     status: usersAccountStatuses("status").notNull().default("pending"),
+    role: userAccountsRoles("role").notNull().default("user"),
     createdAt: timestamp("created_at").defaultNow().notNull(),
     deletedAt: timestamp("deleted_at"),
   },
@@ -31,5 +39,9 @@ export const usersAccountsTable = table(
       t.userId,
       t.accountId,
     ),
+    uniqueOwnersAccounts: uniqueIndex("unique_owners_accounts")
+      .on(t.accountId, t.userId, t.role)
+      // cf. https://github.com/drizzle-team/drizzle-orm/issues/3349#issuecomment-2452165710
+      .where(eq(t.role, sql`'owner'`)),
   }),
 )
