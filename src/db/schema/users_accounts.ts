8958336dff6e563870cd00dd81e58f8a0f72cd25
Jake Holman <me@jakeisonline.com>
2024-12-18 20:48:20 +0000

    add type enum to users_accounts

--- src/db/schema/users_accounts.ts
+++ src/db/schema/users_accounts.ts
@@ -1,19 +1,35 @@
 import { usersTable } from "@/db/schema/users"
 import { accountsTable } from "@/db/schema/accounts"
-import { pgTable as table, text, timestamp, uniqueIndex } from "drizzle-orm/pg-core"
+import {
+  pgTable as table,
+  text,
+  timestamp,
+  uniqueIndex,
+  pgEnum,
+} from "drizzle-orm/pg-core"
 
-export const usersAccountsTable = table("users_accounts", {
-  id: text("id")
-    .primaryKey()
-    .$defaultFn(() => crypto.randomUUID()),
-  userId: text("user_id").references(() => usersTable.id),
-  accountId: text("account_id").references(() => accountsTable.id),
-  createdAt: timestamp("created_at").defaultNow().notNull(),
-  deletedAt: timestamp("deleted_at"),
-}, (t) => ({
-  uniqueUsersAccounts: uniqueIndex("unique_users_accounts").on(
-    t.userId,
-    t.accountId,
+export const usersAccountStatuses = pgEnum("user_account_statuses", [
+  "pending",
+  "active",
+  "inactive",
+])
+
+export const usersAccountsTable = table(
+  "users_accounts",
+  {
+    id: text("id")
+      .primaryKey()
+      .$defaultFn(() => crypto.randomUUID()),
+    userId: text("user_id").references(() => usersTable.id),
+    accountId: text("account_id").references(() => accountsTable.id),
+    status: usersAccountStatuses("status").notNull().default("pending"),
+    createdAt: timestamp("created_at").defaultNow().notNull(),
+    deletedAt: timestamp("deleted_at"),
+  },
+  (t) => ({
+    uniqueUsersAccounts: uniqueIndex("unique_users_accounts").on(
+      t.userId,
+      t.accountId,
     ),
-  })
+  }),
 )
