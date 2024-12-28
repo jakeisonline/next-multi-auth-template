45281cf14c763c02ccc9e5028921d8d321c2c6fe
Jake Holman <me@jakeisonline.com>
2024-12-28 22:12:37 +0000

    feat: allow optional filtering of roles when fetching users

--- src/actions/account/fetch-users.ts
+++ src/actions/account/fetch-users.ts
@@ -4,12 +4,20 @@ import { db } from "@/db"
 import { usersTable } from "@/db/schema/users"
 import { usersAccountsTable } from "@/db/schema/users_accounts"
 import { UUID } from "@/lib/types"
-import { eq } from "drizzle-orm"
+import { and, eq, inArray } from "drizzle-orm"
 
-export async function fetchUsers(accountId: UUID) {
+export async function fetchUsers(
+  accountId: UUID,
+  roles?: ("admin" | "user" | "owner")[],
+) {
   return db
     .select()
     .from(usersTable)
     .innerJoin(usersAccountsTable, eq(usersAccountsTable.userId, usersTable.id))
-    .where(eq(usersAccountsTable.accountId, accountId))
+    .where(
+      and(
+        eq(usersAccountsTable.accountId, accountId),
+        roles ? inArray(usersAccountsTable.role, roles) : undefined,
+      ),
+    )
 }
